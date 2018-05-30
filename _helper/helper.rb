7c9c43f9bdf377ad262cdbfd5f91ef27b7792922
Yuki <yuki08281993@gmail.com>
2018-05-30 16:13:19 +0900

    convert index.html into index.haml manually

--- _helper/helper.rb
+++ _helper/helper.rb
@@ -1,5 +1,9 @@
+def font_dir
+    'https://fonts.googleapis.com/earlyaccess'
+end
+
 def img(a, s, t)
-    "%img{ alt:#{a}, src:#{s}, title:#{t} }"
+    "<img alt=\"#{a}\", src=\"#{s}\", title=\"#{t}\">"
 end
 
 def font_path(name)
